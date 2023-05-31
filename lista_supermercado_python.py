23af58f100e1bc276b49148d43b274abd9c9c828
Victoria Saldaña <victoria1236@gmail.com>
2023-05-30 20:02:32 -0400

    add fix in implementation while

--- lista_supermercado_python.py
+++ lista_supermercado_python.py
@@ -202,7 +202,8 @@ def receive_menu():
             5. Congelados
             6. Bebestibles
             7. Frutas y Verduras
-            0. Volver a mostrar el menú
+            8. Generar Boleta
+            0. Volver a mostrar el menú    
         """))
 
         if menu_section == 1:
@@ -219,6 +220,8 @@ def receive_menu():
             get_list_bebestibles(bebestibles)
         elif menu_section == 7:
             get_list_frutas_verdura(frutas_verduras)
+        elif menu_section == 8:
+            generar_boleta()
         elif menu_section == 0:
             continue  # Reiniciar el bucle y mostrar el menú nuevamente
         else:
@@ -227,20 +230,14 @@ def receive_menu():
         break  # Salir del bucle while después de mostrar una sección
 
 
-    
-    
-
-     
-
-
-suma_total_productos_panaderia = 0
-suma_total_lacteos = 0
-total_carne = 0 
-total_articulos_aseo = 0
-total_alimentos_congelados = 0
-total_bebestibles = 0
-total_frutas_verduras = 0
-total_acumulado = 0
+#suma_total_productos_panaderia = 0
+#suma_total_lacteos = 0
+#total_carne = 0 
+#total_articulos_aseo = 0
+#total_alimentos_congelados = 0
+#total_bebestibles = 0
+#total_frutas_verduras = 0
+#total_acumulado = 0
 
 
 # Sección 1 Panaderia:   
@@ -278,34 +275,34 @@ def get_list_panaderia(panaderia, tortas):
             1. Sí
             2. No
             """))
-        
-        while True:
-            for posicion, i in enumerate(tortas):
-                pos = posicion + 1
-                producto = i["tipo_torta"]
-                precio = i["precio_torta"]
-                print(f"{pos}. {producto}, precio: {precio}")
+        if opcionTorta == 1: 
+            while True:
+                for posicion, i in enumerate(tortas):
+                    pos = posicion + 1
+                    producto = i["tipo_torta"]
+                    precio = i["precio_torta"]
+                    print(f"{pos}. {producto}, precio: {precio}")
 
-            opcion_torta_elegida = int(input("Ingrese el número de opción de torta que desea: "))
+                opcion_torta_elegida = int(input("Ingrese el número de opción de torta que desea: "))
 
-            if opcion_torta_elegida >= 1 and opcion_torta_elegida <= len(tortas):
-                cantidad = int(input("Ingrese la cantidad de tortas que desea llevar: "))
-                total_tortas = tortas[opcion_torta_elegida - 1]['precio_torta'] * cantidad
-                print(f"Total Tortas: {total_tortas}")
-                break
-            else:
-                print("Ingrese una opción válida.\n")
-        else:
+                if opcion_torta_elegida >= 1 and opcion_torta_elegida <= len(tortas):
+                    cantidad = int(input("Ingrese la cantidad de tortas que desea llevar: "))
+                    total_tortas = tortas[opcion_torta_elegida - 1]['precio_torta'] * cantidad
+                    print(f"Total Tortas: {total_tortas}")
+                    break
+                else:
+                    print("Ingrese una opción válida.\n")      
+        else: 
             opcion_torta_elegida = 0
             total_tortas = 0
             print("No agrega Tortas")
-
-    else: 
+            receive_menu()
+    else:
         receive_menu()
         
-    suma_total_productos_panaderia = total_pan + total_tortas
+    total_panaderia = total_pan + total_tortas
 
-    return suma_total_productos_panaderia
+    return total_panaderia
 
 # Seccion 2 Lacteos
 
@@ -341,6 +338,7 @@ def get_list_lacteos(leches, yogures, quesos):
             opcion_leche_elegida = 0
             total_leches = 0
             print("No agrega leche")
+            #receive_menu()
 
     # Yogures
 
@@ -348,65 +346,62 @@ def get_list_lacteos(leches, yogures, quesos):
             1. Sí
             2. No
             """))
-    
-        while True:
-            for posicion, i in enumerate(yogures):
-                pos = posicion + 1
-                producto = i["tipo_yogurt"]
-                marca = i["marca"]
-                precio = i["precio_yogurt"]
-                print(f"{pos}. {producto}, marca: {marca}, precio: {precio}")
+        if opcionYogurt == 1: 
+            while True:
+                for posicion, i in enumerate(yogures):
+                    pos = posicion + 1
+                    producto = i["tipo_yogurt"]
+                    marca = i["marca"]
+                    precio = i["precio_yogurt"]
+                    print(f"{pos}. {producto}, marca: {marca}, precio: {precio}")
 
-            opcion_yogurt_elegido = int(input("Ingrese el número de opción de yogurt que desea: "))
+                opcion_yogurt_elegido = int(input("Ingrese el número de opción de yogurt que desea: "))
 
-            if opcion_yogurt_elegido >= 1 and opcion_yogurt_elegido <= len(yogures):
-                cantidad = int(input("Ingrese la cantidad de yogurt que desea llevar: "))
-                total_yogurt = yogures[opcion_yogurt_elegido - 1]['precio_yogurt'] * cantidad
-                print(f"Total Yogurt: {total_yogurt}")
-                break
-            else:
-                print("Ingrese una opción válida.\n")
+                if opcion_yogurt_elegido >= 1 and opcion_yogurt_elegido <= len(yogures):
+                    cantidad = int(input("Ingrese la cantidad de yogurt que desea llevar: "))
+                    total_yogurt = yogures[opcion_yogurt_elegido - 1]['precio_yogurt'] * cantidad
+                    print(f"Total Yogurt: {total_yogurt}")
+                    break
+                else:
+                    print("Ingrese una opción válida.\n")
         else:
             opcion_yogurt_elegido = 0
             total_yogurt = 0
             print("No agrega Yogurt")
+            receive_menu()
 
     # Quesos
         opcionQueso = int(input(""" ¿Desea añadir Queso?
             1. Sí
             2. No
             """))
-         
-        while True: 
-            for posicion, i in enumerate(quesos):
-                pos = posicion + 1
-                producto = i["tipo_queso"]
-                marca = i["marca"]
-                precio = i["precio_queso"]
-                print(f"{pos}. {producto}, marca: {marca}, precio: {precio}")
+        if opcionQueso == 1: 
+            while True: 
+                for posicion, i in enumerate(quesos):
+                    pos = posicion + 1
+                    producto = i["tipo_queso"]
+                    marca = i["marca"]
+                    precio = i["precio_queso"]
+                    print(f"{pos}. {producto}, marca: {marca}, precio: {precio}")
 
-            opcion_queso_elegido = int(input("Ingrese el número de opción de queso que desea: "))
+                opcion_queso_elegido = int(input("Ingrese el número de opción de queso que desea: "))
 
-            if opcion_queso_elegido >= 1 and opcion_queso_elegido <= len(quesos):
-                cantidad = int(input("Ingrese la cantidad de queso que desea llevar: "))
-                total_queso = quesos[opcion_queso_elegido - 1]['precio_queso'] * cantidad
-                print(f"Total Queso: {total_queso}")
-                break
-            else:
-                print("Ingrese una opción válida.\n")
+                if opcion_queso_elegido >= 1 and opcion_queso_elegido <= len(quesos):
+                    cantidad = int(input("Ingrese la cantidad de queso que desea llevar: "))
+                    total_queso = quesos[opcion_queso_elegido - 1]['precio_queso'] * cantidad
+                    print(f"Total Queso: {total_queso}")
+                    receive_menu()
+                else:
+                    print("Ingrese una opción válida.\n")
         else:
             opcion_queso_elegido = 0
             total_queso = 0
             print("No agrega queso")
+            receive_menu()
     
-    else:
-        receive_menu()
-
-    suma_total_lacteos = total_leches + total_queso + total_yogurt
-    
-    total_acumulado = total_acumulado + suma_total_lacteos
-    return suma_total_lacteos
-
+    total_lacteos = total_leches + total_queso + total_yogurt
+ 
+    return total_lacteos
 
 #Seccion 3 Carnes
     
@@ -435,18 +430,15 @@ def get_list_carniceria(carnes):
                 cantidad = int(input("Ingrese la cantidad de carne que desea llevar: "))
                 total_carne = carnes[opcion_carne_elegida - 1]['precio_kilo'] * cantidad
                 print(f"Total Carne: {total_carne}")
-                break
+                
+                receive_menu()
             else:
                 print("Ingrese una opción válida.\n")
-
-        else:
-            opcion_carne_elegida = 0
-            total_carne = 0
-            print("No agrega carne")
     else:
+        opcion_carne_elegida = 0
+        total_carne = 0
         receive_menu()
 
-    total_acumulado = total_acumulado + total_carne
     return total_carne
 
 # Seccion 4 Aseo
@@ -475,18 +467,15 @@ def get_list_aseo(aseo):
                 cantidad = int(input("Ingrese la cantidad de artículos de aseo que desea llevar: "))
                 total_articulos_aseo = aseo[opcion_articulo_elegido - 1]['precio_unidad'] * cantidad
                 print(f"Total Artículos de aseo: {total_articulos_aseo}")
-                break
+                receive_menu()
             else:
-                print("Ingrese una opción válida.\n")
-                    
-        else:
-            opcion_articulo_elegido = 0
-            total_articulos_aseo = 0
-            print("No agrega Artículos de aseo")
+                print("Ingrese una opción válida.\n")                  
     else:
+        opcion_articulo_elegido = 0
+        total_articulos_aseo = 0
+        print("No agrega Artículos de aseo")
         receive_menu()
-   
-    total_acumulado = total_acumulado + total_articulos_aseo
+ 
     return total_articulos_aseo
 
 # Seccion 5 Congelados
@@ -515,17 +504,15 @@ def get_list_congelados(congelados):
                 cantidad = int(input("Ingrese la cantidad de alimentos congelados que desea llevar: "))
                 total_alimentos_congelados = congelados[opcion_congelado_elegido - 1]['valor'] * cantidad
                 print(f"Total Alimentos congelados: {total_alimentos_congelados}")
-                break
+                receive_menu()
             else:
                 print("Ingrese una opción válida.\n")
-        else:
-            opcion_congelado_elegido = 0
-            total_alimentos_congelados = 0
-            print("No agrega Alimentos congelados")
     else:
+        opcion_congelado_elegido = 0
+        total_alimentos_congelados = 0
+        print("No agrega Alimentos congelados")
         receive_menu()
-        
-    total_acumulado = total_acumulado + total_alimentos_congelados
+    
     return total_alimentos_congelados
 
 # Sección 6 Bebestibles
@@ -554,17 +541,15 @@ def get_list_bebestibles(bebestibles):
                 cantidad = int(input("Ingrese la cantidad de bebestibles que desea llevar: "))
                 total_bebestibles = bebestibles[opcion_bebestible_elegido - 1]['valor_unidad'] * cantidad
                 print(f"Total bebestibles: {total_bebestibles}")
-                break
+                receive_menu()
             else:
                 print("Ingrese una opción válida.\n")
-        else:
-            total_bebestibles = 0
-            total_bebestibles = 0
-            print("No agrega bebestibles")    
     else:
+        opcion_bebestible_elegido = 0
+        total_bebestibles = 0
+        print("No agrega bebestibles")   
         receive_menu()
         
-    total_acumulado = total_acumulado + total_bebestibles
     return total_bebestibles
 
 def get_list_frutas_verdura(frutas_verduras):
@@ -594,14 +579,12 @@ def get_list_frutas_verdura(frutas_verduras):
                 break
             else:
                 print("Ingrese una opción válida.\n")
-        else:
-            opcion_frutas_verduras = 0
-            total_frutas_verduras = 0
-            print("No agrega frutas ni verduras")
     else:
+        opcion_frutas_verduras = 0
+        total_frutas_verduras = 0
+        print("No agrega frutas ni verduras")
         receive_menu()
             
-    total_acumulado = total_acumulado + total_frutas_verduras
     return total_frutas_verduras
 
 
@@ -615,46 +598,37 @@ def generar_boleta():
     total_bebestibles = get_list_bebestibles(bebestibles)
     total_frutas_verduras = get_list_frutas_verdura(frutas_verduras)
 
-def get_payment():
-        # Calcular total general
-        
-    #lista_valores = [ total_panaderia,
-     #   total_lacteos,
-      #  total_carniceria,
-       # total_aseo,
-        #total_congelados,
-        #total_bebestibles,
-        #total_frutas_verduras,
-   # ]
+    # Calcular total general
+    lista_valores = [ total_panaderia,
+        total_lacteos,
+        total_carniceria,
+        total_aseo,
+        total_congelados,
+        total_bebestibles,
+        total_frutas_verduras,
+    ]
 
+    total_general = sum(lista_valores)
 
-    total_general = total_acumulado
     iva = int(total_general * 0.19)
-    total_final = total_general + iva
 
-    suma_total_productos_panaderia = 0
-    suma_total_lacteos = 0
-    total_carne = 0 
-    total_articulos_aseo = 0
-    total_alimentos_congelados = 0
-    total_bebestibles = 0
-    total_frutas_verduras = 0
+    total_final = total_general + iva
     
     # Imprimir boleta
     print("\n \n")
     print("======= BOLETA ELECTRONICA RUT 81.678.873-0=======")
     print("Sección Panadería:")
-    print(f"Total Panadería: {suma_total_productos_panaderia}")
+    print(f"Total Panadería: {total_panaderia}")
     print("Sección Lácteos:")
-    print(f"Total Leches: {suma_total_lacteos}")
-    print(f"Total Yogurts: {suma_total_lacteos}")
-    print(f"Total Quesos: {suma_total_lacteos}")
+    print(f"Total Leches: {total_lacteos}")
+    print(f"Total Yogurts: {total_lacteos}")
+    print(f"Total Quesos: {total_lacteos}")
     print("Sección Carnicería:")
-    print(f"Total Carnicería: {total_carne}")
+    print(f"Total Carnicería: {total_carniceria}")
     print("Sección Aseo:")
-    print(f"Total Artículos de Aseo: {total_articulos_aseo}")
+    print(f"Total Artículos de Aseo: {total_aseo}")
     print("Sección Congelados:")
-    print(f"Total Alimentos Congelados: {total_alimentos_congelados}")
+    print(f"Total Alimentos Congelados: {total_congelados}")
     print("Sección Bebestibles:")
     print(f"Total Bebestibles: {total_bebestibles}")
     print("Sección Frutas y Verduras:")
@@ -664,7 +638,6 @@ def get_payment():
     print(f"Total IVA 19%: {iva}")
     print(f"TOTAL: {total_final} ")
     metodos_pago_boleta(total_final)
-  
 
 def metodos_pago_boleta(total_final):
     total_metodo_pago = total_final
